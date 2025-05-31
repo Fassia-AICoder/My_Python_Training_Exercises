c6e28033c6836731d3653fca392bff6b765be9d0
Fassia-AICoder <fassiaouanre@gmail.com>
2025-05-31 17:49:50 +0000

    The fist exercises of the indermediate level

---
+++ ExercisesIntermediate.py
@@ -0,0 +1,151 @@
+#Author : Fassiatou OUANRE
+#This is my collection of Python training exercises for intermediate level.
+#These exercises are written in simple English for beginners.
+#Because my native language is French, and I am still learning English,
+#But my English in this file is an improvement over my first file (BeginnerExercises).
+
+"""After these exercises I will be able to do any project I want in python """      
+
+                                #These exercises focus on working with characters.
+
+
+#Exercise 1: Write a function that prints “Python is fun!”.
+
+"""def fun():
+    print("Python is fun!")
+fun()"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                #These exercises focus on working with numbers
+
+#Exercise 2:  Write a function that takes a number and returns its triple.
+
+"""def numbers(num):
+    return  num*3
+triple = numbers(3)
+print(triple)"""
+
+#Exercise 2: Write a function that takes two numbers and returns their difference.
+"""def numbers(num1,num2):
+    return num1 - num2
+dif = numbers(2,3)
+print(dif)"""
+
+#Exercise 2:  Write a function that takes a list and returns its length.
+"""def function(l):
+    return len(l)
+
+
+t = function([1, 3, 8, 9])
+print(t)"""
